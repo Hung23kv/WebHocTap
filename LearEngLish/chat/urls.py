cd4c80cc3182ef37b5b92e0668c73d555f2ff720
Hùng Trịnh <trinhhung23102003@gmail.com>
2025-05-05 09:33:33 +0700

    chuc nang tro chuyen

--- LearEngLish/chat/urls.py
+++ LearEngLish/chat/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.homeChat,name='homeChat'),
-    path('chat/<int:idQ>',views.ChatDetail,name = 'detailChat'),
+    path('chat/<int:id>',views.ChatDetail,name = 'detailChat'),
+    path('save-back/',views.save,name = 'save'),
+    path('creatChat/',views.save,name = 'create_chat'),
 ]
--- LearEngLish/chat/views.py
+++ LearEngLish/chat/views.py
@@ -1,57 +1,89 @@
 from django.shortcuts import render,redirect
 from .models import Doithoai,Thoai,Traloi,Nguoidung
 from datetime import date
-from django.db.models import Q
+import json
 
 # Create your views here.
 def homeChat(request):
     data = Doithoai.objects.all()
     return render(request,'ChatHome.html',{"Listchat": data})
-def ChatDetail(request, idQ):
+def ChatDetail(request, id):
+    dataDoiThoai = Doithoai.objects.get(id=id)
+    ngay = date.today().strftime('%Y-%m-%d')
+    id_user = 2 
+    cauhois = Thoai.objects.filter(id_doithoai=id).order_by('id')
     
-    dataDoiThoai = Doithoai.objects.get(id=idQ)
-    
-   
-    cauhois = Thoai.objects.filter(id_doithoai=idQ).order_by('id')
-    
-    current_index = request.session.get('current_index', 0)
-    reply = request.session.get('reply', [])
+    current_index = int(request.POST.get('current_index', 0))
+    reply_json = request.POST.get('reply_data', '[]')
+    reply = json.loads(reply_json)
 
     if request.method == 'POST':
         user_answer = request.POST.get('ChatContent')
-
         if current_index < cauhois.count():
             current_question = cauhois[current_index]
-
+            
             reply.append({
                 'question': current_question.cauhoi,
-                'answer': user_answer
+                'id_user': id_user,
+                'answer': user_answer,
+                'time': ngay,
+                'id_question': current_question.id,
             })
-
-           
-            request.session['reply'] = reply
-            request.session['current_index'] = current_index + 1
-
-           
-            if current_index + 1 < cauhois.count():
-                return redirect('detailChat', idQ=idQ)
-            else:
-                request.session.pop('reply', None)
-                request.session.pop('current_index', None)
+            
+            current_index += 1
+            
+            if current_index < cauhois.count():
                 return render(request, 'ChatDetail.html', {
                     "Chat": dataDoiThoai,
                     "chat": reply,
+                    "Content": cauhois[current_index],
+                    "done": False,
+                    "current_index": current_index,
+                    "reply_data": json.dumps(reply),
+                    "remaining_questions": cauhois.count() - current_index
+                })
+            else:
+                return render(request, 'ChatDetail.html', {
+                    "Chat": dataDoiThoai, 
+                    "chat": reply,
                     "Content": None,
-                    "done": True
+                    "done": True,
+                    "reply_data": json.dumps(reply)
                 })
 
-    current_question = None
-    if current_index < cauhois.count():
-        current_question = cauhois[current_index]
-
+    # Khởi tạo ban đầu
     return render(request, 'ChatDetail.html', {
         "Chat": dataDoiThoai,
-        "chat": reply,
-        "Content": current_question,
-        "done": False
-    })
+        "chat": [],
+        "Content": cauhois[0] if cauhois else None,
+        "done": False,
+        "current_index": 0,
+        "reply_data": json.dumps([]),
+        "remaining_questions": cauhois.count()
+})
+def save(request):
+    if request.method == 'POST':
+        data = json.loads(request.POST.get('reply_data', '[]'))
+        for item in data:
+            id_user = item['id_user']
+            id_question = item['id_question']
+            answer = item['answer']
+            time = item['time']
+            
+            thoai = Thoai.objects.get(id=id_question)
+            nguoi_dung = Nguoidung.objects.get(id=id_user)
+            
+            # Lưu câu trả lời vào cơ sở dữ liệu
+            traloi = Traloi(id_nguoidung=nguoi_dung, id_cauhoi=thoai,ngaygui=time, noidung=answer)
+            traloi.save()
+            
+        return redirect('homeChat')
+    return redirect('homeChat')
+def creatChat(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        purpose = "Trò chuyện"
+        doiThoai = Doithoai(ten=title, mucdich=purpose)
+        doiThoai.save()
+        return redirect('homeChat')
+    return render(request, 'creatChat.html')
